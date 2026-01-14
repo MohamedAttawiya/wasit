0175b3b5479dd6a310055818398a3a024fd1fe9a
Attawiya <attawiya@amazon.com>
2026-01-14 03:28:28 +0200

    adding datastack

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,20 +1,19 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib/core';
-import { InfraStack } from '../lib/infra-stack';
+import * as cdk from "aws-cdk-lib";
+import { ObservabilityStack } from "../lib/ObservabilityStack";
+import { DataStack } from "../lib/DataStack";
 
 const app = new cdk.App();
-new InfraStack(app, 'InfraStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+new ObservabilityStack(app, "wasit-dev-observability", {
+  env: {
+    account: "336814728114",
+    region: "eu-central-1",
+  },
+  prefix: "wasit-dev",
+});
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new DataStack(app, "wasit-dev-data", {
+  env: { account: "336814728114", region: "eu-central-1" },
+  prefix: "wasit-dev",
 });
